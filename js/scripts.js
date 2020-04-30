b595cd4923205cfac4d5a03f3bd3478a3289e031
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-04-30 11:35:41 +0300

    add index.html file with header and script link, also add js/ascipts.js containing the add substract and multiply functions

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+var add = function(number1, number2) {
+  return number1 + number2
+};
+var number1 = parseInt(prompt("Enter a number:"));//i enter 4
+var number2 = parseInt(prompt("Enter another number:"));//i enter 8
+alert("result " + add(number1, number2));
+
+
+var substract = function(number1, number2) {
+  return number1 - number2
+};
+var number1 = parseInt(prompt("Enter a number:"));//i will enter 9
+var number2 = parseInt(prompt("Enter another number:"));//i will enter 4
+alert("result " + substract(number1, number2));
+
+var multiply = function(number1, number2) {
+  return number1 * number2
+};
+var number1 = parseInt(prompt("Enter a number:"));// i enter 5
+var number2 = parseInt(prompt("Enter another number:"));// i ener 7
+alert("result " + multiply(number1, number2));
