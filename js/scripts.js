3b323b526df120204fb6a58d09a78efe6b0052d1
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-05-07 10:10:12 +0300

    add multiply and divide code

--- js/scripts.js
+++ js/scripts.js
@@ -35,3 +35,24 @@ $(document).ready(function(){
     $("#output").text(result);
   });
 });
+
+
+$(document).ready(function(){
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = (multiply(number1, number2));
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function(){
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = (divide(number1, number2));
+    $("#output").text(result);
+  });
+});
