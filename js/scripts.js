c92e39979fede6961e18e9d9bb4b5958129ef4b9
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-05-07 06:41:26 +0300

    add user interface logic code

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
+//business (or back-end)logic
 var add = function(number1, number2) {
   return number1 + number2
 };
-var number1 = parseInt(prompt("Enter a number:"));//i enter 4
-var number2 = parseInt(prompt("Enter another number:"));//i enter 8
-alert("result " + add(number1, number2));
-
 
 var substract = function(number1, number2) {
   return number1 - number2
 };
-var number1 = parseInt(prompt("Enter a number:"));//i will enter 9
-var number2 = parseInt(prompt("Enter another number:"));//i will enter 4
-alert("result " + substract(number1, number2));
 
 var multiply = function(number1, number2) {
   return number1 * number2
 };
-var number1 = parseInt(prompt("Enter a number:"));// i enter 5
-var number2 = parseInt(prompt("Enter another number:"));// i ener 7
-alert("result " + multiply(number1, number2));
-
 
 var divide = function(number1, number2) {
   return number1 / number2
 };
-var number1 = parseInt(prompt("Enter a number:"));// i enter 10
-var number2 = parseInt(prompt("Enter another number:"));// i ener 2
-alert("result " + divide(number1, number2));
+
+//Everything below this line is user interface(or front-end)logic
+$(document).ready(function(){
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
